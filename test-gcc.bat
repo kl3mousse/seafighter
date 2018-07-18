a2a4933b69724cba254c04caff787fd848820cd8
kl3mousse <kl3mousse@gmail.com>
2018-07-18 14:10:06 +0200

    cleaning temp files

--- test-gcc.bat
+++
@@ -1,7 +0,0 @@
-@REM cd include\NeoDev\m68k\bin
-
-@REM gcc -I..\include -m68000 -O3 -Wall -fomit-frame-pointer -ffast-math -fno-builtin -nostartfiles -nodefaultlibs -D__cart__ -c ..\..\..\..\main.c -o ..\..\tmp\main.o
-
-@REM cd ..\..\..\..
-
-gcc -Iinclude\NeoDev\m68k\include -m68000 -O3 -Wall -fomit-frame-pointer -ffast-math -fno-builtin -nostartfiles -nodefaultlibs -D__cart__ -c main.c -o include\NeoDev\tmp\main.o
